881e9ff696a2dab25d819c340f42d26cef0d8601
Pierre Rudloff <contact@rudloff.pro>
2016-04-16 13:18:23 +0200

    Use dicollecte-lexicon to get suffixes

--- classes/Converter.php
+++ classes/Converter.php
@@ -3,27 +3,47 @@
 namespace Epíkoinos;
 
 use Stringy\Stringy as S;
+use Dicollecte\Lexicon;
 
 class Converter
 {
 
     private $separator;
-    private $suffixes = array(
-        'teur'=>'rice',
-        'if'=>'ive',
-    );
 
     public function __construct($separator = '.')
     {
         $this->separator = $separator;
+        $this->lexicon = new Lexicon(__DIR__.'/../vendor/dicollecte/lexique/lexique-dicollecte-fr-v5.6.txt');
     }
 
     private function convertWordObject(S $w)
     {
-        foreach ($this->suffixes as $suffix => $add) {
-            if ($w->endsWith($suffix)) {
-                $w = $w->ensureRight($this->separator.$add);
+        $inflections = $this->lexicon->getByLemma($w);
+        foreach ($inflections as $inflection) {
+            if ($inflection->inflection == $w
+                && $inflection->hasTag('nom')
+                && $inflection->hasTag('mas')
+            ) {
+                $isPlural = $inflection->hasTag('pl');
+                break;
             }
+        }
+        if (!isset($isPlural)) {
+            return $w;
+        }
+        foreach ($inflections as $inflection) {
+            if ($inflection->hasTag('nom')
+                && ($isPlural && $inflection->hasTag('pl') || !$isPlural && $inflection->hasTag('sg'))
+                && $inflection->hasTag('fem')
+            ) {
+                $femInflection = $inflection;
+                break;
+            }
+        }
+        if (isset($femInflection)) {
+            $prefix = $w->longestCommonPrefix($femInflection->inflection);
+            $suffix = S::create($femInflection->inflection)->removeLeft($prefix);
+            $w = $w->ensureRight($this->separator.$suffix);
         }
         return $w;
     }
