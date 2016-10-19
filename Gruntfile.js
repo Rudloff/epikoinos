1de49311c9f453a0d06ca1666509959ac1fd05ce
Pierre Rudloff <contact@rudloff.pro>
2016-10-19 13:59:29 +0200

    Deploy with Shipit

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,6 +8,9 @@ module.exports = function (grunt) {
     grunt.loadNpmTasks('grunt-jsonlint');
     grunt.loadNpmTasks('grunt-fixpack');
     grunt.loadNpmTasks('grunt-phpdocumentor');
+    grunt.loadNpmTasks('grunt-shipit');
+    grunt.loadNpmTasks('shipit-git-update');
+    grunt.loadNpmTasks('shipit-composer-simple');
 
     grunt.initConfig({
         jslint: {
@@ -57,10 +60,20 @@ module.exports = function (grunt) {
                     directory: 'classes/,tests/'
                 }
             }
+        },
+        shipit: {
+            prod: {
+                deployTo: '/var/www/epikoinos/',
+                servers: 'pierre@dev.rudloff.pro',
+                composer: {
+                    noDev: true
+                }
+            }
         }
     });
 
     grunt.registerTask('lint', ['jslint', 'fixpack', 'jsonlint', 'phpcs']);
     grunt.registerTask('test', ['phpunit']);
     grunt.registerTask('doc', ['phpdocumentor']);
+    grunt.registerTask('prod', ['shipit:prod', 'update', 'composer:install']);
 };
