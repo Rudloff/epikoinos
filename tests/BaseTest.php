ba2fb03e5d33ded2dcd3ce7fe046162506196eac
Pierre Rudloff <contact@rudloff.pro>
2017-06-25 20:23:46 +0200

    Add link to test words

--- tests/BaseTest.php
+++ tests/BaseTest.php
@@ -24,6 +24,7 @@ abstract class BaseTest extends TestCase
 
     /**
      * Return a list of words to use for tests.
+     * Based on page 34 of http://www.haut-conseil-egalite.gouv.fr/IMG/pdf/hcefh__guide_pratique_com_sans_stereo-_vf-_2015_11_05.pdf
      *
      * @return array[]
      */
